1734d58fde511dcde7393680723b227bf728bb56
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-03-20 15:12:44 +0700

    membuat input untuk user dan menambahkan while

--- Latihan4_157/Latihan4_157.cpp
+++ Latihan4_157/Latihan4_157.cpp
@@ -6,7 +6,7 @@ float LuasPersegi(float p, float l) {
 }
 
 float LuasLingkaran(float r) {
-	return 3.14 * r * r 
+	return 3.14 * r * r;
 }
 
 float LuasSegitiga(float a, float t) {
@@ -19,16 +19,45 @@ int main()
 	int pilihan;
 
 	do {
-		cout << "\n\n===================" << endl;
-		cout << "\n\n====== MENU ======" << endl;
-		cout << "\n\n=================" << endl;
+		cout << "\n\n======================" << endl;
+		cout << "======== MENU ========" << endl;
+		cout << "======================" << endl;
 		cout << "1. Luas Persegi" << endl;
 		cout << "2. Luas Lingkaran" << endl;
 		cout << "3. Luas Segitiga" << endl;
 		cout << "4. Exit" << endl;
+		cout << "Pilihan (1/2/3/4) : ";
 		cin >> pilihan;
 
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukkan Panjang = ";
+			cin >> panjang;
+			cout << "Masukkan Lebar";
+			cin >> lebar;
+			cout << "Luas Persegi Panjang = " << LuasPersegi(panjang, lebar);
+			break;
+		case 2:
+			cout << "Masukkan jari-jari = ";
+			cin >> jejari;
+			cout << "Luas Lingkaran = " << LuasLingkaran(jejari);
+			break;
+		case 3:
+			cout << "Masukkan Alas = ";
+			cin >> alas;
+			cout << "Masukkan Tinggi = ";
+			cin >> tinggi;
+			cout << "Luas Segitiga = " << LuasSegitiga(alas, tinggi);
+		case 4:
+			break;
+		default:
+			cout << "PILIHAN SALAH" << endl;
+			break;
+		}
+
+
+	} while (pilihan != 4);
 	}
 
-}
 
